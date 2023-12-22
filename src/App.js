0094d528f4a2ffe786d95c539b04085c0d2066f9
Nadine Grantz <nadine.grantz@googlemail.com>
2023-12-22 15:34:30 +0100

    add solutio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Greeting name="Hase" />
+    </div>
+  );
+}
+
+// das hier ist eine component
+function Greeting({ name }) {
+  return (
+    <div>
+      <p>Hello {name}</p>
+    </div>
+  );
 }
